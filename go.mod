9d579013ab6809c1c51c2308e01f96af84ab0cb3
hezhaowu <hezhaowu@kingsoft.com>
2019-07-30 22:20:14 +0800

    save

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.1
-	github.com/obase/center v0.1.0
+	github.com/obase/center v0.1.1
 	github.com/obase/conf v0.11.3
 )
--- http.go
+++ http.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"context"
-	"github.com/obase/conf"
 	"github.com/obase/center"
+	"github.com/obase/conf"
 	"io"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,6 +29,34 @@ var defaultClient = &http.Client{
 	Timeout: conf.OptiDuration("http.requestTimeout", 60*time.Second),
 }
 
-func HttpPost(ctx context.Context, serviceName string, uri string, header *http.Header, body io.Reader) (state int, rsp string, err error) {
+func HttpPost(ctx context.Context, serviceName string, uri string, header *http.Header, body io.Reader) (state int, content string, err error) {
+	service, err := center.Robin(serviceName)
+	if err != nil {
+		return
+	}
+	// 拼接URL
+	buf := new(strings.Builder)
+	buf.WriteString("http://")
+	buf.WriteString(service.Host)
+	buf.WriteString(":")
+	buf.WriteString(strconv.Itoa(service.Port))
+	buf.WriteString(uri)
 
+	// 创建请求
+	req, err := http.NewRequest(http.MethodPost, buf.String(), body)
+	if err != nil {
+		return
+	}
+	rsp, err := defaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer rsp.Body.Close()
+
+	buf.Reset()
+	_, err = io.Copy(buf, rsp.Body)
+	if err != nil {
+		return
+	}
+	return rsp.StatusCode, buf.String(), nil
 }
